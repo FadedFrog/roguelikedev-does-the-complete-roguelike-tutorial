45a054a8a7cabd50b465fe27d0ab44a92dbe69b5
FadedFrog <fadedfroggaming@gmail.com>
2017-06-30 05:12:35 -0400

    This makes me current up to video 3 of the tutorial. I had an issue that took me a bit of time to figure out that I was missing a closing ).

--- main_game.py
+++ main_game.py
@@ -7,28 +7,77 @@ import constants
 
 
 
+#      _______.___________..______       __    __    ______ .___________.
+#     /       |           ||   _  \     |  |  |  |  /      ||           |
+#    |   (----`---|  |----`|  |_)  |    |  |  |  | |  ,----'`---|  |----`
+#     \   \       |  |     |      /     |  |  |  | |  |         |  |     
+# .----)   |      |  |     |  |\  \----.|  `--'  | |  `----.    |  |     
+# |_______/       |__|     | _| `._____| \______/   \______|    |__|         
 
+class struc_Tile:
+	def __init__(self, block_path):
+		self.block_path = block_path                                                                       
+
+
+
+
+
+# .___  ___.      ___      .______   
+# |   \/   |     /   \     |   _  \  
+# |  \  /  |    /  ^  \    |  |_)  | 
+# |  |\/|  |   /  /_\  \   |   ___/  
+# |  |  |  |  /  _____  \  |  |      
+# |__|  |__| /__/     \__\ | _|     
+
+def map_create():
+	new_map = [[ struc_Tile(False) for y in range(0, constants.MAP_HEIGHT)] for x in range(0, constants.MAP_WIDTH) ]
+
+	new_map[10][10].block_path = True
+	new_map[10][15].block_path = True
+
+	return new_map
+                                   
+
+
+
+
+
+# _______  .______          ___   ____    __    ____  __  .__   __.   _______ 
+# |       \ |   _  \        /   \  \   \  /  \  /   / |  | |  \ |  |  /  _____|
+# |  .--.  ||  |_)  |      /  ^  \  \   \/    \/   /  |  | |   \|  | |  |  __  
+# |  |  |  ||      /      /  /_\  \  \            /   |  | |  . `  | |  | |_ | 
+# |  '--'  ||  |\  \----./  _____  \  \    /\    /    |  | |  |\   | |  |__| | 
+# |_______/ | _| `._____/__/     \__\  \__/  \__/     |__| |__| \__|  \______| 
+                                                                             
 def draw_game():
-
-	global SURFACE_MAIN
 
 	#clear the surface
 	SURFACE_MAIN.fill(constants.COLOR_DEFAULT_BG)
 
-	#TODO draw the map
-
+	#draw the map
+	draw_map(GAME_MAP)
 	#draw the character
-	SURFACE_MAIN.blit(constants.S_PLAYER, ( 200, 200 ))
+	SURFACE_MAIN.blit(constants.S_PLAYER, (200, 200))
 
-	#TODO update the display
+	# update the display
 	pygame.display.flip()
 
+def draw_map(map_to_draw):
 
-
-
-
-
-
+	for x in range(0,constants.MAP_WIDTH):
+		for y in range(0,constants.MAP_HEIGHT):
+			if map_to_draw[x][y].block_path == True:
+				#draw wall
+				SURFACE_MAIN.blit(constants.S_WALL, (x*constants.CELL_WIDTH, y*constants.CELL_HEIGHT))
+			else:
+				SURFACE_MAIN.blit(constants.S_FLOOR, (x*constants.CELL_WIDTH, y*constants.CELL_HEIGHT))
+#   _______      ___      .___  ___.  _______ 
+#  /  _____|    /   \     |   \/   | |   ____|
+# |  |  __     /  ^  \    |  \  /  | |  |__   
+# |  | |_ |   /  /_\  \   |  |\/|  | |   __|  
+# |  |__| |  /  _____  \  |  |  |  | |  |____ 
+#  \______| /__/     \__\ |__|  |__| |_______|
+                                           
 def game_main_loop():
 	'''In this function, we loop the main game'''
 	game_quit = False
@@ -50,23 +99,43 @@ def game_main_loop():
 	pygame.quit()
 	exit()
 
-
-
-
-
-
-
 def game_initialize():
 	'''This function initializes the main window, and pygame'''
 
-	global SURFACE_MAIN
+	global SURFACE_MAIN, GAME_MAP
 
 	#initialize pygame
 	pygame.init()
 
 	SURFACE_MAIN = pygame.display.set_mode( (constants.GAME_WIDTH, constants.GAME_HEIGHT) )
 
+	GAME_MAP = map_create()
 
+
+
+
+#            ::'.:                       
+#             ''M;                       
+#             """:;                       
+#                 M;.     .               
+#        ...... ..M:'    MM;.             
+#      ;MMMMMMMMMM:     :MMMM.           
+#      MMMMMMMMMMMMM..  ;MMMM;.           
+#      'MMMMMMMMMMMMMM;.MMM'MM;           
+#       '"MMMMMMMMMMMMMMMMM 'MM;     ,. . 
+# , ,:    ':MMMMMMMMMMMMMMM  'MM.   :;'"" 
+# "'.:     :M:":MMMMMMMMMMM   'M; ,;:;;"" 
+# '"'M;..;.;M'  ""MMMMMMMM:    'M.MMMM:"" 
+# '"'  ""'""" ;.MMMMMMMM""      '"""'     
+#          ,;MMMMMMM:""                   
+#          'MMMMMMM;.;.                   
+#             """'""""":M..               
+#                       ,MM               
+#   -hrr-              ;MM:               
+#                    ,;'MM'               
+#                    ":"M:               
+#                    ::::.  
+##EXECUTE GAME##
 if __name__ == '__main__':
 	game_initialize()
 	game_main_loop()
