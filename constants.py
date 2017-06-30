59304c1aa32aafa6793081e161a5899ce98b520e
FadedFrog <fadedfroggaming@gmail.com>
2017-06-29 21:02:32 -0400

    I'm attempting to work my way throught the videos for the Python 2.7 + Pygame + Libtcod roguelike tutorials. I have no prior programming experience and am not sure that I'll be able to go beyond the confines of the turorial on my own.

---
+++ constants.py
@@ -0,0 +1,18 @@
+import pygame
+
+pygame.init()
+
+#Game sizes
+GAME_WIDTH = 800
+GAME_HEIGHT = 600
+
+#Color Definitions
+COLOR_BLACK = (0, 0, 0)
+COLOR_WHITE = (255, 255, 255)
+COLOR_GREY = (100, 100, 100)
+
+#Game Colors
+COLOR_DEFAULT_BG = COLOR_GREY
+
+#SPRITES
+S_PLAYER = pygame.image.load("data/python.png")
---
+++ main_game.py
@@ -0,0 +1,72 @@
+#3rd party modules
+import libtcodpy as libtcod
+import pygame
+
+#game files
+import constants
+
+
+
+
+def draw_game():
+
+	global SURFACE_MAIN
+
+	#clear the surface
+	SURFACE_MAIN.fill(constants.COLOR_DEFAULT_BG)
+
+	#TODO draw the map
+
+	#draw the character
+	SURFACE_MAIN.blit(constants.S_PLAYER, ( 200, 200 ))
+
+	#TODO update the display
+	pygame.display.flip()
+
+
+
+
+
+
+
+def game_main_loop():
+	'''In this function, we loop the main game'''
+	game_quit = False
+
+	while not game_quit:
+
+		#get player input
+		events_list = pygame.event.get()
+
+		#TODO process input
+		for event in events_list:
+			if event.type == pygame.QUIT:
+				game_quit = True
+
+		# draw the game
+		draw_game()
+
+	#quit the game
+	pygame.quit()
+	exit()
+
+
+
+
+
+
+
+def game_initialize():
+	'''This function initializes the main window, and pygame'''
+
+	global SURFACE_MAIN
+
+	#initialize pygame
+	pygame.init()
+
+	SURFACE_MAIN = pygame.display.set_mode( (constants.GAME_WIDTH, constants.GAME_HEIGHT) )
+
+
+if __name__ == '__main__':
+	game_initialize()
+	game_main_loop()
